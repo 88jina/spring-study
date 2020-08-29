d839632c0b52d9ffc6290859db6d67e1ab8f0994
88jina <jina940323@gmail.com>
2020-08-29 17:23:14 +0900

    spring intial setting

---
+++ SpringTemp/src/com/spring/mvc/configuration/ServletAppConfiguration.java
@@ -0,0 +1,23 @@
+package com.spring.mvc.configuration;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.mvc.*;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(value = "com.example",useDefaultFilters = false, 
+							 includeFilters = @ComponentScan.Filter(Controller.class))
+public class ServletAppConfiguration implements WebMvcConfigurer{
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+        registry.addResourceHandler("/**").addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        WebMvcConfigurer.super.configureViewResolvers(registry);
+        registry.jsp("/views/", ".jsp");
+    }
+}
